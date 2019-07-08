0cfc8b958fa814c81179b4f2d2694943fd281420
MAPersonGit <anon4math@gmail.com>
2019-07-08 17:55:48 +0400

    second step

--- src/components/EditTask.tsx
+++ src/components/EditTask.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import { Link, Route } from "react-router-dom";
-
+import React, {useState} from 'react';
+import { Link } from "react-router-dom";
 import {task} from '../types';
 
 
 export function EditTask({tasks, onClick, match}:any) {
     const task = tasks.find((task:task) => task.id == match.params.taskID)
+    const [formValue, setFormValue] = useState(task.title)
+    const [buttonBehavior, setButtonBehavior] = useState({title: 'remove', action: "DELETE"});
+
+    function inputHandler(e:any) {
+        const inputValue = e.currentTarget.value;
+
+        setFormValue(inputValue);
+
+        if ( inputValue === task.title ) {
+            setButtonBehavior({title: 'back to list', action: ""});
+        } else {
+            setButtonBehavior({title: 'save', action: "EDIT"});
+        }
+    }
+
+
+
     return (
         <div>
             <h1>{`Задача № ${task.id}`}</h1>
             <form>
             <label htmlFor="taskInput">Краткое описание</label>
-                <input id="taskInput" type="text"/>
+                <input id="taskInput" type="text" value={formValue} onChange={inputHandler}/>{task.title}
             </form>
-            <button onClick={() => onClick(task.id, "DELETE")}>удалить</button>
-            <Link to='/'>back to list</Link>
+            <Link to='/'>
+                <button onClick={() => onClick(task.id, buttonBehavior.action)}>{buttonBehavior.title}</button>
+            </Link>
         </div>
     )
 }
--- src/components/Taskslist.tsx
+++ src/components/Taskslist.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {List} from './List';
 import {EditTask} from './EditTask'
